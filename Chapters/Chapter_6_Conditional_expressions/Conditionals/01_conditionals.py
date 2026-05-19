0abe73099036158d1de02884c11ca801f25453b5
Sameer Pankaj <sameer.karnaa@gmail.com>
2026-05-19 21:27:44 +0200

    added cahpter 6 and files

---
+++ Chapters/Chapter_6_Conditional_expressions/Conditionals/01_conditionals.py
@@ -0,0 +1,12 @@
+a = int(input("Enter your age: "))
+
+if a >= 18:
+    print("You are an adult.")   
+    print("You can vote and drive.")
+
+else:
+    print("You are a minor.")
+    print("You cannot vote or drive.")
+
+
+print('End of program.  ')
