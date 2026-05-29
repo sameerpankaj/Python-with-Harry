0be08eea0fb4a397e5f49da03571100ce6b3b44d
Sameer Pankaj <sameer.karnaa@gmail.com>
2026-05-29 15:02:08 +0200

    added project 1

---
+++ Projects/Project1/Snake_Water_Gun_Game/main.py
@@ -0,0 +1,38 @@
+'''
+1 for snake
+-1 for water
+0 for gun
+
+'''
+
+import random
+
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice:")
+youDict = {"s": 1, "w": -1, "g": 0}
+reverseDict = {1: "snake", -1: "water", 0: "gun"}
+
+you = youDict[youstr]
+
+#By now we have 2 numbers(variables), you and computer
+
+print(f"You chose {reverseDict[you]}\nComputer Chose {reverseDict[computer]}")
+
+if computer == you:
+    print("It's a tie")
+else:   
+    if computer == -1 and you == 1:
+        print("You win")
+    elif computer == -1 and you == 0:
+        print("You lose")
+    elif computer == 1 and you == -1:
+        print("You lose")
+    elif computer == 1 and you == 0:
+        print("You win")
+    elif computer == 0 and you == -1:
+        print("You win")    
+    elif computer == 0 and you == 1:
+        print("You lose")
+    else:
+        print("Something went wrong!")
+
---
+++ Projects/Project1/Snake_Water_Gun_Game/readme.txt
@@ -0,0 +1,2 @@
+#We all have played snake, water gun game in our childhood. If you haven't, google the rules of this game and write a python
+#program capable of playing this game with the user.
