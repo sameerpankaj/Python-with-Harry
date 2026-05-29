1049fd0ed0a2d5c81f370fe89030dba4a6d9b07d
Sameer Pankaj <sameer.karnaa@gmail.com>
2026-05-29 15:19:23 +0200

    added and modified files in project 1

--- Projects/Project1/Snake_Water_Gun_Game/main_shortened.py
+++ Projects/Project1/Snake_Water_Gun_Game/main_shortened.py
@@ -1,3 +1,5 @@
+#This program is same as main.py but with shortened version
+
 '''
 1 for snake
 -1 for water
@@ -17,34 +19,39 @@ you = youDict[youstr]
 #By now we have 2 numbers(variables), you and computer
 
 print(f"You chose {reverseDict[you]}\nComputer Chose {reverseDict[computer]}")
+if computer == you:
+    print("It's a tie")
+else:
+    if(computer - you) == -1 or (computer - you) == 2:
+      print('You lose')
+    else:
+     print('You win')
 
+
+''' 
 if computer == you:
     print("It's a tie")
 else:   
-    if computer == -1 and you == 1:
+    if computer == -1 and you == 1: (computer - you) = -2
         print("You win")
-    elif computer == -1 and you == 0:
+    elif computer == -1 and you == 0:(computer - you) = -1
         print("You lose")
-    elif computer == 1 and you == -1:
+    elif computer == 1 and you == -1:(computer - you) = 2
         print("You lose")
-    elif computer == 1 and you == 0:
+    elif computer == 1 and you == 0:(computer - you) = 1
         print("You win")
-    elif computer == 0 and you == -1:
+    elif computer == 0 and you == -1: (computer - you) = 1
         print("You win")    
-    elif computer == 0 and you == 1:
+    elif computer == 0 and you == 1:(computer - you) = 1
         print("You lose")
     else:
         print("Something went wrong!")
 
+    The below logic is written on the basis of the value of (computer - you)
 
 '''
-or
 
-if(computer - you) == -1 or (computer - you) == 2):
-    print('You lose')
 
-else:
-    print('You win')
 
-'''
+
 
